d77680228cacd7713750bec05803c7e6f02d1722
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-04-25 18:14:24 -0500

    refactor: create class Card within its own module

---
+++ scripts/Card.js
@@ -0,0 +1,66 @@
+import {openModal} from './index.js';
+
+const imageModal = document.querySelector('.modal_type_image');
+const imageModalImage = imageModal.querySelector('.modal__image');
+const imageModalCaption = imageModal.querySelector('.modal__image-caption');
+
+export class Card {
+
+  static selectors = {
+    template: "#element-template",
+    elements: ".elements__list",
+    element: ".element",
+    elementImage: ".element__image",
+    elementTitle: ".element__title",
+    deleteButton: ".element__delete-btn",
+    likeButton: ".element__like-btn",
+    likeButtonActive: "element__like-btn_active"
+  }
+
+  constructor(data, cardSelector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._alt = data.alt;
+    this._cardSelector = cardSelector;
+  }
+
+  _getTemplate () {
+    const newElement = document
+    .querySelector(this._cardSelector)
+    .content
+    .querySelector(Card.selectors.element)
+    .cloneNode(true);
+
+    return newElement;
+  }
+
+  _handleLike() {
+    this._element.querySelector(Card.selectors.likeButton).classList.toggle(Card.selectors.likeButtonActive);
+  }
+
+  _handleImageClick() {
+    imageModalImage.src = this._link;
+    imageModalCaption.textContent = this._name;
+    imageModalImage.alt = this._alt;
+    openModal(imageModal);
+  }
+
+  _setEventListeners() {
+    this._element.querySelector(Card.selectors.deleteButton).addEventListener('click', () => this._element.remove())
+    this._element.querySelector(Card.selectors.likeButton).addEventListener('click', () => this._handleLike());
+    this._element.querySelector(Card.selectors.elementImage).addEventListener('click', () => this._handleImageClick());
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+
+    this._setEventListeners();
+  
+    this._element.querySelector(Card.selectors.elementImage).src = this._link;
+    this._element.querySelector(Card.selectors.elementTitle).textContent = this._name;
+    this._element.querySelector(Card.selectors.elementImage).alt = this._alt;
+  
+    return this._element;
+  }
+}
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,95 @@
+import {Card} from './Card.js';
+
+// variables
+
+const editModal = document.querySelector('.modal_type_edit');
+const addModal = document.querySelector('.modal_type_add');
+
+const profile = document.querySelector(".profile");
+const openEditModalBtn = profile.querySelector(".profile__edit-btn");
+const openAddModalBtn = profile.querySelector(".profile__add-btn");
+const profileName = profile.querySelector(".profile__name");
+const profileBio = profile.querySelector(".profile__bio");
+
+const elements = document.querySelector('.elements__list');
+
+const editForm = document.querySelector(".edit-form");
+const editFormInputName = editForm.querySelector(".form__item_el_name");
+const editFormInputBio = editForm.querySelector(".form__item_el_bio");
+
+const addForm = document.querySelector(".add-form");
+const addFormInputName = addForm.querySelector(".form__item_el_name");
+const addFormInputLink = addForm.querySelector(".form__item_el_link");
+const addFormSubmitButton = addForm.querySelector(".form__submit-btn");
+
+// functions
+
+export const openModal = ((modal) => {
+  modal.classList.add("modal_opened");
+  document.addEventListener('keydown', closeModalWithEscButton);
+  setOverlayClickEventListener(modal);
+});
+
+const setOverlayClickEventListener = ((modal) => {
+  modal.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('modal') || evt.target.classList.contains('modal__close-btn')) {
+      closeModal(modal);
+    }
+  })
+});
+
+const closeModal = ((modal) => {
+  modal.classList.remove("modal_opened");
+  document.removeEventListener('keydown', closeModalWithEscButton);
+});
+
+const closeModalWithEscButton = ((evt) => {
+  const openedModal = document.querySelector('.modal_opened');
+  if (evt.key === "Escape") {
+    closeModal(openedModal);
+  }
+});
+
+const editFormSubmitHandler = ((evt) => {
+  evt.preventDefault();
+  profileName.textContent = editFormInputName.value;
+  profileBio.textContent = editFormInputBio.value;
+  closeModal(editModal);
+});
+
+const addFormSubmitHandler = ((evt) => {
+  evt.preventDefault();
+  // add a new element to elements
+  const element = new Card({name: addFormInputName.value, link: addFormInputLink.value, alt: `Изображение ${addFormInputName.value}`}, '#element-template');
+  const cardElement = element.generateCard();
+  elements.prepend(cardElement);
+
+  addForm.reset();
+  disableFormSubmitButton(addFormSubmitButton);
+  closeModal(addModal);
+});
+
+const disableFormSubmitButton = (button) => {
+  button.classList.add("form__submit-btn_disabled");
+  button.setAttribute("disabled", true);
+}
+
+// event listeners
+
+editForm.addEventListener('submit', editFormSubmitHandler);
+addForm.addEventListener('submit', addFormSubmitHandler);
+
+openEditModalBtn.addEventListener ('click', function () {
+  openModal(editModal);
+  editFormInputName.value = profileName.textContent;
+  editFormInputBio.value = profileBio.textContent;
+});
+
+openAddModalBtn.addEventListener ("click", () => openModal(addModal));
+
+
+// when page opens, there should be 6 elements added by JavaScript
+
 const initialCards = [
   {
     name: 'Laramie, WY',
@@ -31,137 +123,8 @@ const initialCards = [
   }
 ];
 
-// variables
-
-const editModal = document.querySelector('.modal_type_edit');
-const addModal = document.querySelector('.modal_type_add');
-const imageModal = document.querySelector('.modal_type_image');
-
-const profile = document.querySelector(".profile");
-const openEditModalBtn = profile.querySelector(".profile__edit-btn");
-const openAddModalBtn = profile.querySelector(".profile__add-btn");
-const profileName = profile.querySelector(".profile__name");
-const profileBio = profile.querySelector(".profile__bio");
-
-const elements = document.querySelector('.elements__list');
-
-const editForm = document.querySelector(".edit-form");
-const editFormInputName = editForm.querySelector(".form__item_el_name");
-const editFormInputBio = editForm.querySelector(".form__item_el_bio");
-
-const addForm = document.querySelector(".add-form");
-const addFormInputName = addForm.querySelector(".form__item_el_name");
-const addFormInputLink = addForm.querySelector(".form__item_el_link");
-const addFormSubmitButton = addForm.querySelector(".form__submit-btn");
-
-const imageModalImage = imageModal.querySelector('.modal__image');
-const imageModalCaption = imageModal.querySelector('.modal__image-caption');
-
-// functions
-
-const openModal = ((modal) => {
-  modal.classList.add("modal_opened");
-  document.addEventListener('keydown', closeModalWithEscButton);
-  setOverlayClickEventListener(modal);
+initialCards.forEach((item) => {
+  const element = new Card(item, Card.selectors.template);
+  const cardElement = element.generateCard();
+  document.querySelector(Card.selectors.elements).append(cardElement);
 });
-
-const setOverlayClickEventListener = ((modal) => {
-  modal.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('modal') || evt.target.classList.contains('modal__close-btn')) {
-      closeModal(modal);
-    }
-  })
-});
-
-const closeModal = ((modal) => {
-  modal.classList.remove("modal_opened");
-  document.removeEventListener('keydown', closeModalWithEscButton);
-});
-
-const closeModalWithEscButton = ((evt) => {
-  const openedModal = document.querySelector('.modal_opened');
-  if (evt.key === "Escape") {
-    closeModal(openedModal);
-  }
-});
-
-// create an element using template
-const createElement = ((name, link, alt = `Изображение ${name}`) => {
-  const elementTemplate = document.querySelector('#element-template').content;
-  const element = elementTemplate.cloneNode(true);
-  const elementImage = element.querySelector('.element__image');
-
-  elementImage.src = link;
-  elementImage.alt = alt;
-  element.querySelector('.element__title').textContent = name;
-
-  element.querySelector('.element__like-btn').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__like-btn_active');
-  })
-
-  element.querySelector('.element__delete-btn').addEventListener('click', function (evt) {
-    evt.target.closest('.element').remove();
-  })
-
-  const imageClickHandler = (() => {
-    imageModalImage.src = link;
-    imageModalCaption.textContent = name;
-    imageModalImage.alt = `Изображение ${name}`;
-    openModal(imageModal);
-  });
-
-  elementImage.addEventListener('click', imageClickHandler);
-  
-  return element;
-});
-
-// add a new element to elements
-const addElement = ((name, link, alt) => {
-  const element = createElement(name, link, alt);
-  elements.append(element);
-});
-
-const editFormSubmitHandler = ((evt) => {
-  evt.preventDefault();
-  profileName.textContent = editFormInputName.value;
-  profileBio.textContent = editFormInputBio.value;
-  closeModal(editModal);
-});
-
-const addFormSubmitHandler = ((evt) => {
-  evt.preventDefault();
-  const newElement = createElement(addFormInputName.value, addFormInputLink.value);
-  elements.prepend(newElement);
-  addForm.reset();
-  disableFormSubmitButton(addFormSubmitButton);
-  closeModal(addModal);
-});
-
-const disableFormSubmitButton = (button) => {
-  button.classList.add("form__submit-btn_disabled");
-  button.setAttribute("disabled", true);
-}
-
-// event listeners
-
-editForm.addEventListener('submit', editFormSubmitHandler);
-addForm.addEventListener('submit', addFormSubmitHandler);
-
-openEditModalBtn.addEventListener ('click', function () {
-  openModal(editModal);
-  editFormInputName.value = profileName.textContent;
-  editFormInputBio.value = profileBio.textContent;
-});
-
-openAddModalBtn.addEventListener ("click", () => openModal(addModal));
-
-
-// when page opens, there should be 6 elements added by JavaScript
-
-const initializePhotos = ((arr) => {
-  arr.forEach((item) => {
-    addElement(item.name, item.link, item.alt);
-  });
-});
-
-initializePhotos(initialCards);

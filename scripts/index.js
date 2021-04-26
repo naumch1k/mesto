b6c5fd05cc53f26f44d70a3c4e19df0ab0cf1e3c
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-04-26 09:20:42 -0500

    Hotfix: remove OverlayClickEventListener when close modal

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,7 @@ const formValidationSettings = {
 
 const editModal = document.querySelector('.modal_type_edit');
 const addModal = document.querySelector('.modal_type_add');
+//const openedModal = document.querySelector('.modal_opened');
 
 const profile = document.querySelector(".profile");
 const openEditModalBtn = profile.querySelector(".profile__edit-btn");
@@ -46,25 +47,27 @@ addFormValidator.enableValidation();
 export const openModal = ((modal) => {
   modal.classList.add("modal_opened");
   document.addEventListener('keydown', closeModalWithEscButton);
-  setOverlayClickEventListener(modal);
-});
-
-const setOverlayClickEventListener = ((modal) => {
-  modal.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('modal') || evt.target.classList.contains('modal__close-btn')) {
-      closeModal(modal);
-    }
-  })
+  modal.addEventListener('click', closeModalWithOverlayClick);
 });
 
 const closeModal = ((modal) => {
   modal.classList.remove("modal_opened");
   document.removeEventListener('keydown', closeModalWithEscButton);
+  modal.removeEventListener('click', closeModalWithOverlayClick);
 });
 
+const getCurrentlyOpenedModal = () => document.querySelector('.modal_opened');
+
 const closeModalWithEscButton = ((evt) => {
-  const openedModal = document.querySelector('.modal_opened');
+  const openedModal = getCurrentlyOpenedModal();
   if (evt.key === "Escape") {
+    closeModal(openedModal);
+  }
+});
+
+const closeModalWithOverlayClick = ((evt) => {
+  const openedModal = getCurrentlyOpenedModal();
+  if (evt.target.classList.contains('modal') || evt.target.classList.contains('modal__close-btn')) {
     closeModal(openedModal);
   }
 });
